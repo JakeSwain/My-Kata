9c5fe9996607b316e13f12a56c8e4179a30158e3
JakeSwain <42880333+JakeSwain@users.noreply.github.com>
2019-04-24 09:29:53 -0400

    Add files via upload
    This program is able to find the derivative of a single one term function by using the power rule

---
+++ hello_world.py
@@ -0,0 +1,91 @@
+#
+# user_adjective1 = input('Give me an adjective:')
+#
+# user_adjective2 = input('Give me another adjective:')
+#
+# while True:
+#     user_noun = input('Now, give me a noun:')
+#     if user_noun=="Jake":
+#         print ("Im not a noun. Names are proper nouns")
+#         continue
+#     else
+#         break
+#
+#
+#
+# while True:
+#     user_verb = input('Also, give me a verb ending in -ing:')
+#     if user_verb=="running"
+#         print("everyone chooses running try something else")
+#         continue
+#     else
+#         break
+#
+#
+#
+#
+# user_adverb = input('Now give me an adverb'):
+#     if user_adverb=="quickly":
+#     print("Wow! Look who paid attnetion in middle school!")
+#     user_adverb2 = input("quickly is nice but can you think of any other ones?:")
+#
+#
+#
+#
+#
+# # string interpolation, using all variables to complete the story
+# # print('Amir used his', user_adjective1,'jumper, to score the first points. Then Jacob showed Amir his', user_adjective2, user_noun,
+# #      'and drove to the basket. Jake and Amir won the game and celebrated by',user_adverb, user_verb,'.')
+# #
+# #
+#
+# count=0
+# time=100
+#
+# while count != time:
+#     count = count + 1
+#     print(count)
+#
+#
+
+# num = 7
+# user_guess = input("guess a number")
+#
+# while num != user_guess:
+#     print("wrong")
+#     continue
+# else:
+#     break
+#     print("congrats")
+print("Play the magic 8-Ball! I'll guess somthing right")
+
+user_question = input("ask me a question:")
+print("prolly not")
+
+user_question2 = input("did I get that right?:")
+
+# if user_question2 == "yes" or "Yes" or " yes" or " Yes" or "Yea" or "yea" or " yea" or " Yea":
+#     print("magic!")
+
+user_input = user_input[0]
+
+
+if user_question2 != "yes" or "Yes" or " yes" or " Yes" or "Yea" or "yea" or " yea" or " Yea":
+    print("Ok ask another question then")
+    user_question3 = input("What else do you have:")
+
+    print("for sure")
+    user_question4 = input("Did I get one right now?")
+
+    if user_question4 == "yes" or "Yes" or " yes" or " Yes" or "Yea" or "yea" or " yea" or " Yea":
+        print("magic!")
+
+    if user_question4 != "yes" or "Yes" or " yes" or " Yes" or "Yea" or "yea" or " yea" or " Yea":
+        print("I give up")
+
+        # while user_question6 != "yes" or "Yes" or " yes" or " Yes" or "Yea" or "yea" or " yea" or " Yea":
+        #
+        #     print("Ok I'll try again")
+        #     user_question5 = input("ask me another one:")
+        #     print("no way")
+        #     user_question6 = input("Did I get it right this time?:")
